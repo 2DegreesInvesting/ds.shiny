f9d464763c547b470225dc4f6b5f77c4c3fd071e
Mauro Lepore <maurolepore@gmail.com>
2021-06-22 07:22:03 -0600

    Examples of input functions

--- basic-ui/app.R
+++ basic-ui/app.R
@@ -1,12 +1,32 @@
+# Separation between front/back
+# shinyapp + TAB
+
+# Common structure, e.g. ?selectInput
+# * `inputID` connects front/back end
+# front: `*Input(inputId = "name", ...)`  # frontend
+# back:              `input$name`
+
+
 # shinyapp + TAB
 library(shiny)
 
+# generates your user interface (the front end)
 ui <- fluidPage(
+  textInput("name", "Free text"),
 
+  sliderInput("num", "Numeric input", value = 0, min = 0, max = 100),
+
+  dateInput("dob", "Date input"),
+
+  selectInput("state", "Limited choices", state.name),
+
+  fileInput("upload", "File uploads"),
+
+  actionButton("click", "Action buttons")
 )
 
+# drives your app’s behaviour (the back end).
 server <- function(input, output, session) {
-
 }
 
 shinyApp(ui, server)
