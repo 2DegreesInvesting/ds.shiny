f973ee97d41f85ea858db2bdec28949314bef0a9
Mauro Lepore <maurolepore@gmail.com>
2021-06-29 07:53:49 -0600

    Basic reactivity

---
+++ reactivity/02_reactivity.R
@@ -0,0 +1,78 @@
+# Recipes ---
+
+# What's going on here?
+
+  output$greeting <- renderText({
+    paste0("Hello ", input$name, "!")
+  })
+
+# > paste together ‘hello’ and the user’s name?
+
+# Wrong because you only issue the instruction once.
+
+# Think of your app as providing Shiny with recipes, not giving it commands.
+
+# Lazy ---
+
+ui <- fluidPage(
+  textInput("name", "What's your name?"),
+  textOutput("greeting")
+)
+server <- function(input, output, session) {
+  output$greeting <- renderText({
+    paste0("Hello ", input$name, "!")
+  })
+}
+shinyApp(ui, server)
+
+# You don't get an error. Why?
+server <- function(input, output, session) {
+  output$grXXting <- renderText({
+    paste0("Hello ", input$name, "!")
+  })
+}
+shinyApp(ui, server)
+
+
+
+# The reactive graph ---
+
+# https://mastering-shiny.org/basic-reactivity.html#the-reactive-graph
+
+# |input>  >output|
+# |name>  >greeting|
+server <- function(input, output, session) {
+  output$greeting <- renderText({
+    paste0("Hello ", input$name, "!")
+  })
+}
+
+# |input>  >reactive-expression>  >output|
+# |name>  >string>  >greeting|
+server <- function(input, output, session) {
+  string <- reactive(paste0("Hello ", input$name, "!"))
+  output$greeting <- renderText(string())
+}
+
+
+
+# Execution order ---
+
+server <- function(input, output, session) {
+  string <- reactive(paste0("Hello ", input$name, "!"))
+  output$greeting <- renderText(string())
+}
+shinyApp(ui, server)
+
+# Will this work?
+server <- function(input, output, session) {
+  output$greeting <- renderText(string())
+  string <- reactive(paste0("Hello ", input$name, "!"))
+}
+shinyApp(ui, server)
+
+
+
+# Why do we need reactive expressions? ----
+
+# https://mastering-shiny.org/basic-reactivity.html#reactive-roadblocks
