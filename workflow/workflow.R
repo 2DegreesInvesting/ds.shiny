5fdaa7299ef9e8ba9769f3da9b7b1e0ccd56e52b
Mauro Lepore <maurolepore@gmail.com>
2021-07-19 16:06:33 -0600

    WIP draft meetup

---
+++ workflow/workflow.R
@@ -0,0 +1,19 @@
+# Worklow basics ----
+# https://mastering-shiny.org/action-workflow.html
+
+# Development workflow
+# Snippets.
+# options(shiny.autoreload = TRUE); shiny::runApp().
+# Views
+
+# Debugging
+# interactive debugger with browser()
+# reactivity with message()
+
+# Packages ----
+# https://mastering-shiny.org/scaling-packaging.html#workflow
+
+# Benefits
+# Workflow
+# Infrastructure with usethis and R CMD check
+# Deploy via app.R
