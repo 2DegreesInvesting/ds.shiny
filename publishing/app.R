481310196ccd98bd5f21e1dc23a80f28eb744563
Mauro Lepore <maurolepore@gmail.com>
2021-08-10 07:09:20 -0600

    Draft publishing demo

---
+++ publishing/app.R
@@ -0,0 +1,12 @@
+library(shiny)
+
+ui <- fluidPage(
+  textInput("name", "What's your name?"),
+  textOutput("greeting")
+)
+
+server <- function(input, output, session) {
+  output$greeting <- renderText(paste("Hello", input$name))
+}
+
+shinyApp(ui, server)
